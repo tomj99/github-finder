de1ecbc5df8fa531f81b9f21eeb82dfc349f257a
DESKTOP-U6R1KNJ\jacqu <jacquestom99@gmail.com>
2021-02-11 04:02:25 -0800

    wip

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>
+        <p className='lead'>
+          The paragraph you are looking for does not exist...
+        </p>
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/components/users/Search.js
@@ -0,0 +1,55 @@
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const [text, setText] = useState('');
+
+  const onChange = e => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (text === '') {
+      //below is a prop that passes in this components state and can be used in the custom component
+      alertContext.setAlert('Please enter something', 'light');
+    } else {
+      //below is a prop that passes in this components state and can be used in the custom component
+      githubContext.searchUsers(text);
+      setText('');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className='btn btn-light btn-block'
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
